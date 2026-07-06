d3cd604a3367d48b8ed3b383b549ddae53698a18
Kangzhen Yang <kangzhenyang@KangzhendeMacBook-Air.local>
2026-07-06 12:05:06 +0800

    Use full SMAC training defaults in OzSTAR submit scripts

--- scripts/ozstar_submit_corridor_hyper_ablation_5m.sh
+++ scripts/ozstar_submit_corridor_hyper_ablation_5m.sh
@@ -7,7 +7,7 @@ export MAPS="${MAPS:-corridor}"
 export MODELS="${MODELS:-rpg_linear_interaction_hypercond rpg_fixed_linear_structured_maker}"
 export SEEDS="${SEEDS:-1 2 3 4 5}"
 export T_MAX="${T_MAX:-5000000}"
-export TEST_INTERVAL="${TEST_INTERVAL:-50000}"
+export TEST_INTERVAL="${TEST_INTERVAL:-10000}"
 export TIME="${TIME:-12:00:00}"
 
 exec "$SCRIPT_DIR/ozstar_submit_variant_matrix.sh"
